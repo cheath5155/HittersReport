225625667504cff4201207a56916bd05e1ef8579
cheath5155 <cmheath2303@yahoo.com>
2022-10-04 22:52:12 -0700

    first commit

---
+++ HittersReport.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+##Create a report that takes in hitter data from a CSV file with muliple
+##Trackman games and generated a PPTX and PDF containing charts and other 
+##Data from the hitter
+
+
+
