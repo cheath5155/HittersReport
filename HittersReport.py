94003c96c587167266bedaa8e730ff52b565088e
cheath5155 <cmheath2303@yahoo.com>
2022-10-04 23:37:18 -0700

    created damage map method

--- HittersReport.py
+++ HittersReport.py
@@ -1,10 +1,16 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from tkinter import filedialog
 
 ##Create a report that takes in hitter data from a CSV file with muliple
 ##Trackman games and generated a PPTX and PDF containing charts and other 
 ##Data from the hitter
-#HI
 
+
+csv_file = pd.read_csv(filedialog.askopenfilename())
+
+def damage_heat_map():
+    
 
 
